3bebfcbb8b7c62d786b79f6ee4bd8318e47d8b86
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-02-07 20:10:40 +0100

    add parameter diameter to the rectangle for make a square.

--- src/graphics/GameGraphicRectangle.java
+++ src/graphics/GameGraphicRectangle.java
@@ -4,30 +4,26 @@ import java.awt.Graphics;
 
 public class GameGraphicRectangle extends GameGraphic {
 
-	public GameGraphicRectangle() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public GameGraphicRectangle(double x, double y, double width, double height) {
-		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GameGraphicRectangle(double x, double y) {
+	public GameGraphicRectangle( int x, int y, int diameter ) {
 		super(x, y);
-		// TODO Auto-generated constructor stub
+		this.setWidth( diameter );
+		this.setHeight( diameter );
 	}
 
-	public GameGraphicRectangle(int x, int y, int width, int height) {
-		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GameGraphicRectangle(int x, int y) {
+	public GameGraphicRectangle( double x, double y, double diameter ) {
 		super(x, y);
-		// TODO Auto-generated constructor stub
+		this.setWidth( ( int ) diameter );
+		this.setHeight( ( int ) diameter );
 	}
+	
+	public GameGraphicRectangle( int x, int y, int width, int height ) {
+		super(x, y, width, height);
+	}
+
+	public GameGraphicRectangle( double x, double y, double width, double height ) {
+		super(x, y, width, height );
+	}
+	
 	
 	@Override
 	public void paint( Graphics g ){
