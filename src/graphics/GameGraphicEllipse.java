6477d2191524e95b1880bb5f429cc4781f05c688
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-02-08 18:26:12 +0100

    changing on construchtor.

--- src/graphics/GameGraphicEllipse.java
+++ src/graphics/GameGraphicEllipse.java
@@ -4,16 +4,24 @@ import java.awt.Graphics;
 
 public class GameGraphicEllipse extends GameGraphic {
 	
+	public GameGraphicEllipse(int x, int y, int diameter) {
+		super(x, y);
+		this.setWidth( diameter );
+		this.setHeight( diameter );
+	}
+	
 	public GameGraphicEllipse(double x, double y, double diameter) {
 		super(x, y);
 		this.setWidth( ( int ) diameter );
 		this.setHeight( ( int ) diameter );
 	}
 	
-	public GameGraphicEllipse(int x, int y, int diameter) {
-		super(x, y);
-		this.setWidth( diameter );
-		this.setHeight( diameter );
+	public GameGraphicEllipse( int x, int y, int width, int height ) {
+		super(x, y, width, height);
+	}
+
+	public GameGraphicEllipse( double x, double y, double width, double height ) {
+		super(x, y, width, height );
 	}
 	
 	@Override
