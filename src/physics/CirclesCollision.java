9742c434278028984ee0017c28b8ab79300aa905
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-02-13 11:26:04 +0100

    add getCollisionPointX() and getCollisionPointY() for calculate the collisions point.

--- src/physics/CirclesCollision.java
+++ src/physics/CirclesCollision.java
@@ -26,9 +26,53 @@ public class CirclesCollision extends Collision {
 	@Override
 	public boolean isCollided(){
 		return 
-				this.getDistance()
-				<= this.getFirstObject().getRadius()
-				+ this.getSecondObject().getRadius();
+				super.getDistance()
+				<= super.getFirstObject().getRadius()
+				+ super.getSecondObject().getRadius();
+	}
+	
+	/**
+	 * Get the collision point between the tow circles along the x-axis.
+	 * @return collision point along the x-axis.
+	 */
+	public double getCollisionPointX(){
+		return 
+				(
+					(
+						super.getFirstObject().getCenterX()
+						* super.getSecondObject().getRadius()
+					)
+					+ (
+						super.getSecondObject().getCenterX()
+						* super.getFirstObject().getRadius()
+					)
+				) 
+				/ (
+					super.getFirstObject().getRadius()
+					+ super.getSecondObject().getRadius()
+				);
+	}
+	
+	/**
+	 * Get the collision point between the tow circles along the y-axis.
+	 * @return collision point along the y-axis.
+	 */
+	public double getCollisionPoingY(){
+		return 
+				(
+					(
+						super.getFirstObject().getCenterY()
+						* super.getSecondObject().getRadius()
+					)
+					+ (
+						super.getSecondObject().getCenterY()
+						* super.getFirstObject().getRadius()
+					)
+				) 
+				/ (
+					super.getFirstObject().getRadius()
+					+ super.getSecondObject().getRadius()
+				);
 	}
 	
 	
