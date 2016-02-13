82e28a0fb41e4d39b28890c1c64227b4cebaefef
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-02-13 10:19:47 +0100

    add BallCollision object for ball collisions.

---
+++ src/physics/BallCollision.java
@@ -0,0 +1,13 @@
+package physics;
+
+import objects.WorldObject;
+
+public class BallCollision extends Collision {
+
+	public BallCollision(WorldObject firstObject, WorldObject secondObject) {
+		super(firstObject, secondObject);
+	}
+	
+	
+
+}
