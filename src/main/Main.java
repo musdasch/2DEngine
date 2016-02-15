4eb7e74a22d08f53b07b37fd3a099ddba575b2d4
Tim Vögtli <tim.voegtli@gibmit.ch>
2016-02-15 23:11:22 +0100

    so

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,11 @@ import objects.WorldObjectBall;
 import physics.CollisionCircles;
 import physics.Vector2D;
 
+/**
+ * It is the test area.
+ * @author Tim Vögtli
+ * @version 0.2
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -18,44 +23,73 @@ public class Main {
 		 */
 		World world = new World();
 		
+		
+		/**
+		 * Create an circle.
+		 */
 		WorldObjectBall ball = new WorldObjectBall(
 				0,
 				0,
 				100,
-				100,
 				new Vector2D( 2, 0 ) 
 		);
 		
+		/**
+		 * Create an oder circle.
+		 */
 		WorldObjectBall ball2 = new WorldObjectBall(
 				1000,
 				0,
 				100,
-				100,
 				new Vector2D( -2.0, 0.2 ) 
 		);
 		
+		/**
+		 * Create an graphic object for the tow balls.
+		 */
 		GameGraphicEllipse ellipse = new GameGraphicEllipse(
 				0,
 				0,
 				100 
 		);
 		
+		/**
+		 * Set the color of the graphic.
+		 */
 		ellipse.setBackgroundColor( new Color( 255, 255, 255 ) );
 		
+		
+		/**
+		 * add the graphic to the tow balls.
+		 */
 		ball.setGraphic( ellipse );
 		ball2.setGraphic( ellipse );
 		
+		/**
+		 * add the tow balls to the world
+		 */
 		world.addWorldObject( ball );
 		world.addWorldObject( ball2 );
 		
-		
+		/**
+		 * Create a collision between the tow balls.
+		 */
 		CollisionCircles collision = new CollisionCircles(ball, ball2);
 		world.addCollision(collision);
 		
+		/**
+		 * Set a gravity to the world
+		 */
 		world.setGravity( new Vector2D( 0, 0.0098 ) );
 		
-		world.setWorldDelay( 100 );
+		/**
+		 * set the world delay of 30 milliseconds for slow motion.
+		 */
+		world.setWorldDelay( 30 );
 		
+		/**
+		 * Create a frame for the world.
+		 */
 		GameFrame frame = new GameFrame( "test",  1200, 600, world );
 	}
 
