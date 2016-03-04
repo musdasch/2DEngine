4188f1aab8a984ac7e097e9221fdd47ac0487d2f
Tim <musdasch@gmail.com>
2016-03-04 16:54:12 +0100

    Some Commits.

--- src/listeners/GameWindowListener.java
+++ src/listeners/GameWindowListener.java
@@ -3,8 +3,17 @@ package listeners;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * The GameWindowListener holds the functions for actions in the game.
+ * @author Tim Vögtli
+ * @version 0.1
+ *
+ */
 public class GameWindowListener extends WindowAdapter {
 	
+	/**
+	 * The Window Closing Function stops the program.
+	 */
 	public void windowClosing(WindowEvent e){
 		 e.getWindow().dispose();
 		 System.exit(0);
